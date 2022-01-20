5b0bdd2ea207debd7847b29477c6d4c2557ad0d2
Drummss <1509172+Drummss@users.noreply.github.com>
2022-01-20 11:11:05 +0000

    refactor: website branding now links to home page

--- components/Global/Navbar.tsx
+++ components/Global/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { 
+import {
   Box,
   Drawer,
   DrawerBody,
@@ -25,98 +25,69 @@ import ColorModeButton from '../ColorMode/ColorModeButton';
 import { HamburgerIcon } from '@chakra-ui/icons';
 
 const NavButtons = () => {
-  return (<>
-    <Link href='https://github.com/Drummss' rel='noreferrer' target='_blank'>
-      <IconButton
-        icon={<GithubLogo/>}
-        aria-label='Github Link'
-      />
-    </Link>
-    <ColorModeButton />
-  </>);
-}
+  return (
+    <>
+      <Link href='https://github.com/Drummss' rel='noreferrer' target='_blank'>
+        <IconButton icon={<GithubLogo />} aria-label='Github Link' />
+      </Link>
+      <ColorModeButton />
+    </>
+  );
+};
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Flex
-      pt={10}
-      pb={14}
-      justifyContent='space-between'
-      alignItems='center'
-    >
-      <Heading
-        size='xl'
-        lineHeight={1.3}
-        bgGradient={useColorModeValue(
-          'linear(to-r, #ff09ab, #f7923f)',
-          'linear(to-r, #de31a3, #de611e)'
-        )}
-        bgClip='text'
-        variant='page-title'
-      >
-        Drumsy.me
-      </Heading>
-      <HStack 
-        spacing={4}
-        display={['none', 'none', 'flex', 'flex']}
-      >
-        <Link href='/'>
-          Home
-        </Link>
+    <Flex pt={10} pb={14} justifyContent='space-between' alignItems='center'>
+      <Link href='/' _hover={{ textDecor: 'none' }}>
+        <Heading
+          size='xl'
+          lineHeight={1.3}
+          bgGradient={useColorModeValue(
+            'linear(to-r, #ff09ab, #f7923f)',
+            'linear(to-r, #de31a3, #de611e)'
+          )}
+          bgClip='text'
+          variant='page-title'
+        >
+          Drumsy.me
+        </Heading>
+      </Link>
+      <HStack spacing={4} display={['none', 'none', 'flex', 'flex']}>
+        <Link href='/'>Home</Link>
         <Tooltip hasArrow label='Not Implmented Yet'>
           <Box _focus={{}}>
-            <Link>
-              Blog
-            </Link>
+            <Link>Blog</Link>
           </Box>
         </Tooltip>
-        <Link href='/my-projects'>
-          My Projects
-        </Link>
+        <Link href='/my-projects'>My Projects</Link>
         <Spacer />
         <NavButtons />
       </HStack>
-      <HStack 
-        spacing={4}
-        display={['flex', 'flex', 'none', 'none']}
-      >
+      <HStack spacing={4} display={['flex', 'flex', 'none', 'none']}>
         <NavButtons />
         <IconButton
           aria-label='Navigation Menu Toggle'
-          icon={<HamburgerIcon/>}
+          icon={<HamburgerIcon />}
           colorScheme='telegram'
           onClick={onOpen}
         />
 
-        <Drawer
-          isOpen={isOpen}
-          placement='top'
-          size='full'
-          onClose={onClose}
-        >
+        <Drawer isOpen={isOpen} placement='top' size='full' onClose={onClose}>
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
-            <DrawerHeader>
-              Menu
-            </DrawerHeader>
+            <DrawerHeader>Menu</DrawerHeader>
 
             <DrawerBody>
-              <VStack
-                spacing={2}
-                fontSize={22}
-                alignItems='start'
-              >
+              <VStack spacing={2} fontSize={22} alignItems='start'>
                 <Link href='/' onClick={onClose}>
                   Home
                 </Link>
                 <Tooltip hasArrow label='Not Implemented Yet' placement='right'>
                   <Box display='inline-block' _focus={{}}>
-                    <Link>
-                      Blog
-                    </Link>
+                    <Link>Blog</Link>
                   </Box>
                 </Tooltip>
                 <Link href='/my-projects' onClick={onClose}>
@@ -129,6 +100,6 @@ const Navbar = () => {
       </HStack>
     </Flex>
   );
-}
+};
 
 export default Navbar;
